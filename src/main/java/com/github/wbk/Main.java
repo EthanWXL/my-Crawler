627ad999d9c2c9f80a0d10e613cce3ee1d1181b6
ethan <1527185002@qq.com>
2019-10-14 17:19:42 +0800

    进行简单的重构 将大块的代码抽取成方法，使它更为易读。

--- src/main/java/com/github/wbk/Main.java
+++ src/main/java/com/github/wbk/Main.java
@@ -17,6 +17,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * @author sentry
+ */
 public class Main {
     private static final String SITES = "https://sina.cn";
 
@@ -25,47 +28,53 @@ public class Main {
         Set<String> processedLinks = new HashSet<>();
         linkPool.add(SITES);
         while (true) {
-
             if (linkPool.isEmpty()) {
                 break;
             }
-
             String link = linkPool.remove(linkPool.size() - 1);
-
             if (processedLinks.contains(link)) {
                 continue;
             }
-            if (link.contains("news.sina.cn")&&!link.contains("passport") || "https://sina.cn".equals(link)) {
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-                if (link.startsWith("//")) {
-                    link = "https:" + link;
-                }
-                System.out.println(link);
-                try {
-                    HttpGet httpGet = new HttpGet(link);
-                    httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36\n");
-                    CloseableHttpResponse response = httpclient.execute(httpGet);
-                    HttpEntity entity1 = response.getEntity();
-                    String html = EntityUtils.toString(entity1);
-                    Document doc = Jsoup.parse(html);
-
-                    Elements links = doc.select("a");
-                    if (!links.isEmpty()) {
-                        for (Element aTag : links) {
-                            linkPool.add(aTag.attr("href"));
-                        }
+            if (isValidLink(link)) {
+                Document doc = getAndParasHtml(link);
+                Elements links = doc.select("a");
+                if (!links.isEmpty()) {
+                    for (Element aTag : links) {
+                        linkPool.add(aTag.attr("href"));
                     }
-                    Elements articleTags = doc.select("article");
-                    if (!articleTags.isEmpty()){
-                        for (Element articleTag : articleTags) {
-                            System.out.println(articleTag.child(0).text());
-                        }
-                    }
-                    processedLinks.add(link);
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                processedLinks.add(link);
             }
         }
     }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        Elements articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                System.out.println(articleTag.child(0).text());
+            }
+        }
+    }
+
+    private static Document getAndParasHtml(String link) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        System.out.println(link);
+
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36\n");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity1 = response.getEntity();
+        String html = EntityUtils.toString(entity1);
+        return Jsoup.parse(html);
+    }
+
+    private static boolean isValidLink(String link) {
+        return link.contains("news.sina.cn") && !link.contains("passport") || "https://sina.cn".equals(link);
+    }
 }
+
